016f2ceef63d0cc3c47c36a1c9890dd640e5e4d8
Leonardo <psilva.leo@gmail.com>
2021-06-19 18:07:55 -0300

    Adding standarScaler and Column filter

--- flowi/components/preprocessing/_preprocessing.py
+++ flowi/components/preprocessing/_preprocessing.py
@@ -1,7 +1,8 @@
-from typing import Any
+from typing import Any, List
 
 import dask.dataframe as dd
-import pandas as pd
+from dask_ml.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from flowi.components.component_base import ComponentBase
 from flowi.experiment_tracking.experiment_tracking import ExperimentTracking
@@ -9,6 +10,34 @@ from flowi.utilities.logger import Logger
 
 from dask_ml.impute import SimpleImputer
 import numpy as np
+
+
+class ColumnFilter(BaseEstimator, TransformerMixin):
+    def __init__(self, columns: List[str], exclude_columns: List[str], transformer):
+        self._columns = columns
+        self._exclude_columns = exclude_columns
+        self._transformer = transformer
+
+        if self._columns and self._exclude_columns:
+            raise ValueError("Columns and exclude_columns cannot be set together")
+
+    def _filter_columns(self, X) -> List[str]:
+        if self._columns:
+            return self._columns
+        elif self._exclude_columns:
+            return X.columns.difference(self._exclude_columns).tolist()
+        return X.columns.tolist()
+
+    def fit(self, X):
+        self._transformer.fit(X[self._filter_columns(X)])
+
+    def transform(self, X):
+        X[self._filter_columns(X)] = self._transformer.transform(X[self._filter_columns(X)])
+        return X
+
+    def inverse_transform(self, X):
+        X[self._filter_columns(X)] = self._transformer.inverse_transform(X[self._filter_columns(X)])
+        return X
 
 
 class Preprocessing(ComponentBase):
@@ -29,6 +58,8 @@ class Preprocessing(ComponentBase):
     def fillna(
         self,
         df: dd.DataFrame,
+        columns: List[str] = None,
+        exclude_columns: List[str] = None,
         missing_values: int or float or str or np.nan or None = np.nan,
         strategy: str = "mean",
         fill_value: str or int or None = None,
@@ -37,6 +68,8 @@ class Preprocessing(ComponentBase):
         Fill NA/NaN values using the specified method.
         docstring copied from dask documentation
         :param df: input dask Dataframe
+        :param columns: columns to apply transform
+        :param exclude_columns: columns excluded on apply transform
         :param missing_values: The placeholder for the missing values. All occurrences of missing_values will be imputed. For pandas’ dataframes with nullable integer dtypes with missing values, missing_values should be set to np.nan, since pd.NA will be converted to np.nan.
         :param strategy: The imputation strategy. mean, median, most_frequent or constant
         :param fill_value: When strategy == “constant”, fill_value is used to replace all occurrences of missing_values. If left to the default, fill_value will be 0 when imputing numerical data and “missing_value” for strings or object data types.
@@ -44,7 +77,33 @@ class Preprocessing(ComponentBase):
         self._logger.debug(f"missing_values: {missing_values}")
         self._logger.debug(f"strategy: {strategy}")
 
-        transformer = SimpleImputer(missing_values=missing_values, strategy=strategy, fill_value=fill_value)
+        base_transformer = SimpleImputer(missing_values=missing_values, strategy=strategy, fill_value=fill_value)
+        transformer = ColumnFilter(columns=columns, exclude_columns=exclude_columns, transformer=base_transformer)
+        transformer.fit(df)
+
+        return transformer.transform(df), transformer
+
+    def standard_scaler(
+        self,
+        df: dd.DataFrame,
+        columns: List[str] = None,
+        exclude_columns: List[str] = None,
+        with_mean: bool = True,
+        with_std: bool = True,
+    ):
+        """
+        Standardize features by removing the mean and scaling to unit variance
+        :param df: input dask Dataframe
+        :param columns: columns to apply transform
+        :param exclude_columns: columns excluded on apply transform
+        :param with_mean: If True, center the data before scaling. This does not work (and will raise an exception) when attempted on sparse matrices, because centering them entails building a dense matrix which in common use cases is likely to be too large to fit in memory.
+        :param with_std: If True, scale the data to unit variance (or equivalently, unit standard deviation).
+        """
+        self._logger.debug(f"with_mean: {with_mean}")
+        self._logger.debug(f"with_std: {with_std}")
+
+        base_transformer = StandardScaler(with_mean=with_mean, with_std=with_std)
+        transformer = ColumnFilter(columns=columns, exclude_columns=exclude_columns, transformer=base_transformer)
         transformer.fit(df)
 
         return transformer.transform(df), transformer
--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -25,19 +25,27 @@ FLOW_CHART = {
             "properties": {
                 "name": "LoadFile",
                 "class": "LoadLocal",
-                "attributes": {
-                    "output_df": "train_df",
-                    "train_path": "iris.csv",
-                    "test_path": "",
-                    "test_split": 0.2,
-                    "file_type": "csv",
-                },
+                "attributes": {"train_path": "iris.csv", "test_path": "", "test_split": 0.2, "file_type": "csv"},
             },
         },
         "node-fillna": {
             "id": "node-fillna",
             "type": "Preprocessing",
             "properties": {"name": "Fillna", "class": "Preprocessing", "attributes": {"strategy": ["mean", "median"]}},
+        },
+        "node-standard-scaler": {
+            "id": "node-standard-scaler",
+            "type": "Preprocessing",
+            "properties": {
+                "name": "StandardScaler",
+                "class": "Preprocessing",
+                "attributes": {
+                    # "columns": [["sepal_length", "sepal_width", "petal_length", "petal_width"]],
+                    "exclude_columns": [["class"]],
+                    "with_mean": True,
+                    "with_std": True,
+                },
+            },
         },
         "node-label-enc": {
             "id": "node-label-enc",
@@ -82,8 +90,12 @@ FLOW_CHART = {
         "link-load-fillna-1": {"from": {"nodeId": "node-load-1"}, "to": {"nodeId": "node-label-enc"}},
         "link-load-fillna-2": {"from": {"nodeId": "node-load-2"}, "to": {"nodeId": "node-label-enc"}},
         "link-label-enc-fillna": {"from": {"nodeId": "node-label-enc"}, "to": {"nodeId": "node-fillna"}},
-        "link-fillna-svc": {"from": {"nodeId": "node-fillna"}, "to": {"nodeId": "node-model-svc"}},
-        "link-fillna-svc2": {"from": {"nodeId": "node-fillna"}, "to": {"nodeId": "node-model-svc2"}},
+        "link-label-fillna-standard-scaler": {
+            "from": {"nodeId": "node-fillna"},
+            "to": {"nodeId": "node-standard-scaler"},
+        },
+        "link-standard-scaler-svc": {"from": {"nodeId": "node-standard-scaler"}, "to": {"nodeId": "node-model-svc"}},
+        "link-standard-scaler-svc2": {"from": {"nodeId": "node-standard-scaler"}, "to": {"nodeId": "node-model-svc2"}},
         "link-svc-accuracy": {"from": {"nodeId": "node-model-svc"}, "to": {"nodeId": "node-metric-accuracy"}},
         "link-svc-accuracy2": {"from": {"nodeId": "node-model-svc2"}, "to": {"nodeId": "node-metric-accuracy2"}},
         "link-accuracy-save": {"from": {"nodeId": "node-metric-accuracy"}, "to": {"nodeId": "node-save"}},
@@ -95,11 +107,11 @@ FLOW_CHART = {
 def test_end_to_end_train(mocker):
     mocker.patch.object(flowi.settings, "FLOW_NAME", "End2End Test Flow")
     mocker.patch.object(flowi.settings, "EXPERIMENT_TRACKING", "MLflow")
-    with mock.patch("flowi.flow_chart.node.Mongo") as node_mongo:
-        with mock.patch("flowi.flow_chart.flow_chart.Mongo") as flow_mongo:
-            node_mongo.assignment = {"_client": pymongo.MongoClient()}
-            flow_mongo.assignment = {"_client": pymongo.MongoClient()}
-            main(["train", "--chart", json.dumps(FLOW_CHART)])
+    # with mock.patch("flowi.flow_chart.node.Mongo") as node_mongo:
+    #     with mock.patch("flowi.flow_chart.flow_chart.Mongo") as flow_mongo:
+    #         node_mongo.assignment = {"_client": pymongo.MongoClient()}
+    #         flow_mongo.assignment = {"_client": pymongo.MongoClient()}
+    main(["train", "--chart", json.dumps(FLOW_CHART)])
     os.remove("saved.csv")
 
 
@@ -127,7 +139,7 @@ PREDICT_DESTINY = {
 def test_end_to_end_predict(mocker):
     mocker.patch.object(flowi.settings, "FLOW_NAME", "End2End Test Flow")
     mocker.patch.object(flowi.settings, "EXPERIMENT_TRACKING", "MLflow")
-    with mock.patch("flowi.flow_chart.node.Mongo") as mongo:
-        mongo.assignment = {"_client": pymongo.MongoClient()}
-        main(["predict", "--source", json.dumps(PREDICT_SOURCE), "--destiny", json.dumps(PREDICT_DESTINY)])
+    # with mock.patch("flowi.flow_chart.node.Mongo") as mongo:
+    #     mongo.assignment = {"_client": pymongo.MongoClient()}
+    main(["predict", "--source", json.dumps(PREDICT_SOURCE), "--destiny", json.dumps(PREDICT_DESTINY)])
     os.remove("saved.csv")
